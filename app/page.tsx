03adb09fcc5f7112c1e9d12be7f7717ed18d7d5f
Taha Shah <tahashah411@gmail.com>
2024-10-09 02:27:30 +0500

    make home page hehe

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <Button>Testing</Button>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Link href="/new" className="p-4 rounded-lg bg-neutral-900 text-white">Add New Question</Link>
     </div>
   );
 }
